375f6220c7690aa2fb026f051ced987cd029ad36
Pavlo <pavlo.com.5@gmail.com>
2020-09-08 17:38:10 +0300

    Updates

---
+++ precache-manifest.e33df345700713bb6ec2866f83aa5a2f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e40f80f409b2d29f63ce0c6cb76f6643",
+    "url": "./index.html"
+  },
+  {
+    "revision": "144ed8c05f431e0733f8",
+    "url": "./static/css/main.d2c0f05f.chunk.css"
+  },
+  {
+    "revision": "5730b7f549b143713f16",
+    "url": "./static/js/2.72e07022.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.72e07022.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "144ed8c05f431e0733f8",
+    "url": "./static/js/main.cf52d0eb.chunk.js"
+  },
+  {
+    "revision": "de889ebc4391832068f6",
+    "url": "./static/js/runtime-main.311b6371.js"
+  },
+  {
+    "revision": "0e23403621e2785661c2a29216fa92ee",
+    "url": "./static/media/done.0e234036.png"
+  },
+  {
+    "revision": "d930f3495051b7c643aec4236ab221f5",
+    "url": "./static/media/notDone.d930f349.png"
+  }
+]);
